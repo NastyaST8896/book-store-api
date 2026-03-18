6d4809bf0f645a1e622a3be6dcc744db9f85c5d6
Anastasia Triapochkina <me@trjapochkina.ru>
2026-03-18 19:08:52 +0300

    add getCartBooks

--- src/controllers/cart.ts
+++ src/controllers/cart.ts
@@ -1,10 +1,11 @@
+import { In } from "typeorm";
 import { BooksInUserCart } from "../db/entities/books-in-user-cart";
 import { Cart } from "../db/entities/cart";
-import { 
-  bookRepository, 
-  booksInUserCartRepository, 
-  cartRepository, 
-  userRepository 
+import {
+  bookRepository,
+  booksInUserCartRepository,
+  cartRepository,
+  userRepository
 } from "../db/repositories/repository";
 import { AppRequestHandler } from "../utils/types";
 
@@ -18,11 +19,11 @@ const addBookInCart: AppRequestHandler = async (req, res) => {
 
   const book = await bookRepository.findOne({
     where: {
-      id: req.body.id
+      id: req.body.bookId
     }
   })
 
-  if(!cart && req.user) {
+  if (!cart && req.user) {
 
     const newCart = new Cart();
 
@@ -47,9 +48,65 @@ const addBookInCart: AppRequestHandler = async (req, res) => {
 
   await booksInUserCartRepository.save(bookInUserCart);
 
-  return res.status(201).json({bookInUserCart, cart});
+  return res.status(201).json({ data: { status: 'ok' } });
 };
 
- 
+const getCartBooks: AppRequestHandler = async (req, res) => {
+  const cart = await cartRepository.findOne({
+    where: {
+      userId: req.user.id,
+      status: false
+    }
+  });
 
-export default { addBookInCart };
+  const books = await booksInUserCartRepository
+    .createQueryBuilder("allBooksInCart")
+    .select("allBooksInCart.bookId")
+    .addSelect("SUM(allBooksInCart.currentPrice)", "totalPrice")
+    .where("allBooksInCart.cartId = :cartId", { cartId: cart.id })
+    .groupBy("allBooksInCart.bookId")
+    .getRawMany();
+
+
+  const cartBooksId = books.map((book) => {
+    return book.allBooksInCart_bookId
+  });
+
+
+  const uniqueBooks = await bookRepository.find({
+    relations: { media: true },
+    where: {
+      id: In(cartBooksId)
+    }
+  })
+
+  const cartBooks = books.map((cartBook) => {
+
+    const book = uniqueBooks.find((book) => {
+      return book.id === cartBook.allBooksInCart_bookId
+    })
+
+    return {
+      id: book.id,
+      title: book.title,
+      author: book.author,
+      price: cartBook.totalPrice,
+      media: book.media?.filePath || '',
+      count: (cartBook.totalPrice * 10 / book.price) / 10
+    }
+  })
+
+  const totalPrice = books.reduce((sum, book) => {
+    return sum + (+book.totalPrice)
+  }, 0);
+
+  return res.status(200).json({
+    data: {
+      books: cartBooks,
+      totalPrice: +(totalPrice.toFixed(2)),
+    }
+  });
+};
+
+
+export default { addBookInCart, getCartBooks };
--- src/routes/cart.ts
+++ src/routes/cart.ts
@@ -3,4 +3,6 @@ import { authenticateToken } from "../utils/helpers";
 import CartController from "../controllers/cart" 
 
 export const CartRouter = Router()
-  .post('/add-book-in-cart', authenticateToken, CartController.addBookInCart)
+  .post('/add-book', authenticateToken, CartController.addBookInCart)
+  .get('/books', authenticateToken, CartController.getCartBooks);
+  
