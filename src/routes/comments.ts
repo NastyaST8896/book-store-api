5ace2984e988dcb1d6729d4ddded13eb77fd8274
Anastasia Triapochkina <me@trjapochkina.ru>
2026-04-22 17:54:17 +0300

    added schemaQueryValidator('get-book-comments')

--- src/routes/comments.ts
+++ src/routes/comments.ts
@@ -1,7 +1,8 @@
 import { Router } from "express";
 import { authenticateToken } from "../utils/helpers";
 import CommentsController from "../controllers/comments"
+import { schemaQueryValidator } from "../schema-query-validator";
 
 export const CommentsRouter = Router()
   .post('/add-comment', authenticateToken, CommentsController.addBookComment)
-  .get('/:id', CommentsController.getBookComments);
+  .get('/:id', schemaQueryValidator('get-book-comments'), CommentsController.getBookComments);
