436f02b9ce149c6446085138b3c60b8b9f981548
Anastasia Triapochkina <me@trjapochkina.ru>
2026-04-23 17:41:37 +0300

    added schemaQueryValidator for comments-notifications

--- src/routes/comment-notifications.ts
+++ src/routes/comment-notifications.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import { authenticateToken } from "../utils/helpers";
 import CommentNotificationsController from "../controllers/comment-notifications";
+import { schemaQueryValidator } from "../schema-query-validator";
 
 export const CommentNotificationsRouter = Router()
   .get(
     '/book-notifications',
     authenticateToken,
+    schemaQueryValidator('get-book-comments-notifications'),
     CommentNotificationsController.getCommentBooksNotifications
   );
